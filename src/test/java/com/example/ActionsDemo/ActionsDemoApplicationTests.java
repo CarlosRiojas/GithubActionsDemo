a4c353ba704cdb37516507a830c567874d481a88
CarlosRiojas <carlos.riojas88@gmail.com>
2025-12-07 12:48:13 -0600

    first commit

---
+++ src/test/java/com/example/ActionsDemo/ActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ActionsDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
